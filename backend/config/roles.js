b474c6e42fdd3062309aa43e9a69d2f4e8313b6d
Aman Singh <coolaman6299@gmail.com>
2025-12-15 19:04:10 +0530

    Setup mongodb database

---
+++ backend/config/roles.js
@@ -0,0 +1,5 @@
+module.exports = {
+  ADMIN: "ADMIN",
+  COMMANDER: "COMMANDER",
+  LOGISTICS: "LOGISTICS"
+};
---
+++ backend/server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => console.log("Server running on 5000"));
