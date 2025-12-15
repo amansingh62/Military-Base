4cec58f9287858990d63ce085fe602816263eee4
Aman Singh <coolaman6299@gmail.com>
2025-12-16 00:24:03 +0530

    Written path for routes in the server file

--- backend/server.js
+++ backend/server.js
@@ -9,4 +9,10 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/purchases", require("./routes/purchaseRoutes"));
+app.use("/api/transfers", require("./routes/transferRoutes"));
+app.use("/api/assignments", require("./routes/assignmentRoutes"));
+app.use("/api/dashboard", require("./routes/dashboardRoutes"));
+
 app.listen(5000, () => console.log("Server running on 5000"));
